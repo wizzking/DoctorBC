46e1f08c14d71a706ff79b5fd1ff20b2d55db8f4
WizzKing <cotafiesta@gmail.com>
2018-04-06 21:37:04 -0600

    login

--- application/views/IniciarSesion.php
+++ application/views/IniciarSesion.php
@@ -3,36 +3,34 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Doctor Baby Control</title>
+  <title>Iniciar Sesion</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
   <link rel="stylesheet" href="<?php echo base_url('assets/AdminLTE/bower_components/bootstrap/dist/css/bootstrap.min.css');?>">
-
+  <!-- Font Awesome -->
   <link rel="stylesheet" href="<?php echo base_url('assets/AdminLTE/bower_components/font-awesome/css/font-awesome.min.css');?>">
-
+  <!-- Ionicons -->
   <link rel="stylesheet" href="<?php echo base_url('assets/AdminLTE/bower_components/Ionicons/css/ionicons.min.css');?>">
-
+  <!-- Theme style -->
   <link rel="stylesheet" href="<?php echo base_url('assets/AdminLTE/dist/css/AdminLTE.min.css');?>">
-
+  <!-- iCheck -->
   <link rel="stylesheet" href="<?php echo base_url('assets/AdminLTE/plugins/iCheck/square/blue.css');?>">
 
+
+  <!-- Google Font -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
-<body class="hold-transition register-page">
-<div class="register-box">
-  <div class="register-logo">
-    <a href="../../index2.html"><b>Registro</b></a>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="../../index2.html"><b>Administrador</b></a>
   </div>
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+    <p class="login-box-msg">Iniciar Sesion</p>
 
-  <div class="register-box-body">
-    <p class="login-box-msg">Ingrese sus Datos</p>
-
-    <form action="../../index.html" method="post">
-      <div class="form-group has-feedback">
-        <input type="text" class="form-control" placeholder="Nombre Completo">
-        <span class="glyphicon glyphicon-user form-control-feedback"></span>
-      </div>
+    <form action="../../index2.html" method="post">
       <div class="form-group has-feedback">
         <input type="email" class="form-control" placeholder="Correo Electronico">
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
@@ -41,25 +39,40 @@
         <input type="password" class="form-control" placeholder="Contraseña">
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
-      <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Repetir Contraseña">
-        <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
+      <div class="row">
+        <div class="col-xs-8">
+          <div class="checkbox icheck">
+            <label>
+              <input type="checkbox"> Recordar Contraseña</label>
+          </div>
+        </div>
+        <!-- /.col -->
+        <div class="col-xs-4">
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Iniciar</button>
+        </div>
+        <!-- /.col -->
       </div>
     </form>
+    <!-- /.social-auth-links -->
+    <a href="#">Recuperar Contraseña</a><br>
 
-
-    <a href="#" class="text-center">Ya cuento con un Usuario</a>
   </div>
-  <!-- /.form-box -->
+  <!-- /.login-box-body -->
 </div>
-<!-- /.register-box -->
 
-<!-- jQuery 3 -->
 <script src="<?php echo base_url('assets/AdminLTE/bower_components/jquery/dist/jquery.min.js');?>"></script>
 <!-- Bootstrap 3.3.7 -->
 <script src="<?php echo base_url('assets/AdminLTE/bower_components/bootstrap/dist/js/bootstrap.min.js');?>"></script>
 <!-- iCheck -->
 <script src="<?php echo base_url('assets/AdminLTE/plugins/iCheck/icheck.min.js');?>"></script>
-
+<script>
+  $(function () {
+    $('input').iCheck({
+      checkboxClass: 'icheckbox_square-blue',
+      radioClass: 'iradio_square-blue',
+      increaseArea: '20%' /* optional */
+    });
+  });
+</script>
 </body>
 </html>
